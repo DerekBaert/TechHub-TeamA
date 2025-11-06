3253d47d2c6b9512007ed9db53e070d7017c1219
Joseph-the-gamemaster <josithgoodwin@gmail.com>
2025-11-06 14:22:03 -0600

    fixed player controller script
    I tinkered with the movement script and also unity made me do stuff again

--- Assets/TechHubStarterPack/Code/PlayerController/WorldForcePlayerDemo.cs
+++ Assets/TechHubStarterPack/Code/PlayerController/WorldForcePlayerDemo.cs
@@ -11,38 +11,68 @@ public class WorldForcePlayerDemo : MonoBehaviour
 
     // Physics Settings
     public float forceAmount = 10f;
+    public float slowDownFactor = 0.5f; // Factor to reduce speed
+    public float slowDownDuration = 2f; // Duration of slowdown
 
     // Physics References
     // Note: I used Private here as we don't want people assigning this value in the editor, 
     //      We will grab the reference with code in the Start Method
     private Rigidbody2D rigid;
 
+    // Add these new fields
+    [SerializeField]
+    private bool isAlive = true;
+
+    public bool IsAlive => isAlive; // Public property to check player state
+
+    // Add this new method to handle collisions
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("TrashObstacle") && isAlive)
+        {
+            StartCoroutine(SlowDown());
+        }
+    }
+
+    private IEnumerator SlowDown()
+    {
+        float originalForceAmount = forceAmount; // Store original speed
+        forceAmount *= slowDownFactor; // Reduce speed
+
+        Debug.Log("Player slowed down!");
+
+        yield return new WaitForSeconds(slowDownDuration); // Wait for 2 seconds
+
+        forceAmount = originalForceAmount; // Restore original speed
+        Debug.Log("Player speed restored!");
+    }
+    
     // Start is called before the first frame update
-    void Start() {
+    void Start() 
+    {
         // This is get a reference to the local Rigidbody
         rigid = GetComponent<Rigidbody2D>();
+
+        // Prevent falling:
+        rigid.gravityScale = 0f; // option A: disable gravity
+        // rigid.constraints = RigidbodyConstraints2D.FreezePositionY; // option B: freeze Y position instead
     }
 
     // Update is called once per frame
-    void Update() {
+    void Update() 
+    {
         xInput = Input.GetAxis("Horizontal");
         yInput = Input.GetAxis("Vertical");
     }
 
     // Physics Update: Provide Forces in this loop instead. 
-    void FixedUpdate() {
+    void FixedUpdate() 
+    {
         // Movement
-        // Add Force to the object in the world direction & orientation. 
-        // This is useful if you want to have a simple control scheme where the input directly 
-        //   relates to the movement in "World Space". This is useful for top down games,
-        //   and can be manipulated into other genres (beat em up, fighting, etc)
-        
-        // In this example, the movement input can be combined into a single Vector
-        Vector2 worldForce = new Vector2(xInput, yInput);
+        // Get horizontal input (e.g., A/D keys or Left/Right arrow keys)
+        float horizontalInput = xInput;
 
-        // The AddForce method will apply a force in a World Orientation, which means it will ignore the
-        // rigidbody's own facing value. Usually when using this, I will lock the rotation constraint, and
-        // handle the art seperately later (to have a sprite face different directions etc)
-        rigid.AddForce(worldForce * forceAmount);
+        // Set horizontal velocity, keep vertical locked at 0 so the player won't fall
+        rigid.linearVelocity = new Vector2(horizontalInput * forceAmount, 0f);
     }
 }
