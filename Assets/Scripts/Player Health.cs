847ca969517d7ac36f14b50873dbd8d0cdcd3c7d
PlzzCallMeMC <jackerilofferman@gmail.com>
2025-11-27 12:43:38 -0800

    Revert "game over"
    This reverts commit ab4920c612dd14e8fe27c96355b856a27966ddf1.

--- Assets/Scripts/Player Health.cs
+++ Assets/Scripts/Player Health.cs
@@ -5,7 +5,6 @@ public class PlayerHealth : MonoBehaviour
     public float currentHealth;
     public float maxHealth;
     public HealthBar healthBar;
-    public UIManager uIManager;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,16 +16,6 @@ public class PlayerHealth : MonoBehaviour
     private void Update()
     {
         healthBar.UpdateHealthBar(currentHealth);
-        if (currentHealth == 0)
-        {
-            PlayerDied();
-        }
-    }
-
-    private void PlayerDied()
-    {
-        LevelManager.instance.GameOver();
-        gameObject.SetActive(false);
     }
 
     public void TakeDamage(float damage)
